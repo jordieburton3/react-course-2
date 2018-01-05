450a278206d3a6c04d6b71f6e4a22425d274d6a2
Jordan Burton <jordieburton3@gmail.com>
2018-01-05 11:39:05 -0500

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'Buddy, I hope you find your dad!' });
 });
 
 const PORT = process.env.PORT || 5000;
